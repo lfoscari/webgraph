ed5df270ec9a6d949803f53e9fbcb463a46900fd
Luigi Foscari <luigi.foscari@icloud.com>
2023-03-07 16:33:23 +0100

    added address-only parameter to the script used to extract data from blockchair tsv

---
+++ bash/blockchair/extract.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -e
+
+# TODO: rewrite
+if [[ "$2" == "" ]]; then
+	echo "$(basename $0) DIR NTHREADS [input|output] [ADDRESS-ONLY]" 1>&2
+	echo "Reads files in DIR and processes them using NTHREADS parallel sorts." 1>&2
+	echo "Files are processed as input files if input is specified or as output files if output is specified." 1>&2
+	echo "Only addresses are extracted if ADDRESS_ONLY is specified" 1>&2
+	echo "FILES MUST END WITH A NEWLINE. Fix them with \"sed -i -e '\$a\\' *\"." 1>&2
+	exit 1
+fi
+
+DIR=$1
+NTHREADS=$2
+TARGET=$3
+ADDRESS_ONLY=$4
+
+if [[ "$TARGET" != "output" && "$TARGET" != "input" ]]; then
+  echo "Target \"$TARGET\" must be either \"input\" or \"output\""
+  exit 1
+fi
+
+function file_ends_with_newline() {
+	[[ $(tail -c1 "$1" | wc -l) -gt 0 ]]
+}
+
+FILES=$(mktemp)
+find $DIR -type f >$FILES
+
+# Check that all files end with a newline
+
+while read FILE; do
+	if ! file_ends_with_newline $FILE; then
+		echo "File $FILE does not end with a newline" 1>&2
+		exit 1
+	fi
+done <$FILES
+
+NFILES=$(cat $FILES | wc -l)
+
+# To avoid empty splits, there must be at least as many threads as files
+
+if (( NFILES < NTHREADS )); then
+	NTHREADS=$NFILES
+	echo "Not enough files: number of threads set to $NFILES" 1>&2
+fi
+
+SPLITBASE=$(mktemp)
+split -n l/$NTHREADS $FILES $SPLITBASE
+SPLITS=$(for file in ${SPLITBASE}?*; do echo $file; done)
+
+for SPLIT in $SPLITS; do
+	mkfifo $SPLIT.pipe
+	if [[ "$TARGET" == "output" ]]; then
+	  if [[ "$ADDRESS_ONLY" != "" ]]; then
+		  (tail -q -n+2 $(cat $SPLIT) | cut -f7,10 | awk '{ if ($2 == 0) print $1 }' | LC_ALL=C sort -S2G >$SPLIT.pipe) &
+		else
+		  (tail -q -n+2 $(cat $SPLIT) | cut -f2,7,10 | awk '{ if ($3 == 0) print $1 "\t" $2 }' | LC_ALL=C sort -S2G >$SPLIT.pipe) &
+		fi
+	elif [[ "$TARGET" == "input" ]]; then
+	  if [[ "$ADDRESS_ONLY" != "" ]]; then
+		  (tail -q -n+2 $(cat $SPLIT) | cut -f7 | LC_ALL=C sort -S2G >$SPLIT.pipe) &
+		else
+		  (tail -q -n+2 $(cat $SPLIT) | cut -f7,13 | awk '{ print $2 "\t" $1 }' | LC_ALL=C sort -S2G >$SPLIT.pipe) &
+    fi
+	fi
+done
+
+LC_ALL=C sort -S2G -m $(for SPLIT in $SPLITS; do echo $SPLIT.pipe; done)
+
+rm -f $FILES
+rm -f ${SPLITBASE}*
