475b0384634731b4ca07e49a652e8e1e7edbedf8
Luigi Foscari <luigi.foscari@icloud.com>
2023-03-08 17:08:29 +0100

    removing old pipes

--- bash/blockchair/addresses.sh
+++ bash/blockchair/addresses.sh
@@ -10,18 +10,16 @@ fi
 INPUTSDIR=$1
 OUTPUTSDIR=$2
 NTHREADS=$(expr $3 / 2)
-
 SCRIPTDIR=$(dirname $(realpath $0))
-FILESDIR=$(dirname $INPUTSDIR)
 
-INPUTSFILE=$(mktemp --tmpdir $FILESDIR/tmp.XXX)
-mkfifo $INPUTSFILE
-bash $SCRIPTDIR/extract.sh $INPUTSDIR $NTHREADS input address >$INPUTSFILE &
+[[ -f $INPUTSDIR/input.pipe ]] && rm $INPUTSDIR/input.pipe
+mkfifo $INPUTSDIR/input.pipe
+bash $SCRIPTDIR/extract.sh $INPUTSDIR $NTHREADS input address >$INPUTSDIR/input.pipe &
 
-OUTPUTSFILE=$(mktemp --tmpdir $FILESDIR/tmp.XXX)
-mkfifo $OUTPUTSFILE
-bash $SCRIPTDIR/extract.sh $OUTPUTSDIR $NTHREADS output address >$OUTPUTSFILE &
+[[ -f $INPUTSDIR/output.pipe ]] && rm $OUTPUTSDIR/output.pipe
+mkfifo $OUTPUTSDIR/output.pipe
+bash $SCRIPTDIR/extract.sh $OUTPUTSDIR $NTHREADS output address >$OUTPUTSDIR/output.pipe &
 
 LC_ALL=C sort -S2G -m $INPUTTMP.pipe $OUTPUTTMP.pipe
 
-rm -f $INPUTSFILE $OUTPUTSFILE
+rm -f $INPUTSDIR/input.pipe $OUTPUTSDIR/output.pipe
