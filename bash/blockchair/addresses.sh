80f070d452d6aad806472ff1f01d2071a4de2d6f
Luigi Foscari <luigi.foscari@icloud.com>
2023-03-08 16:29:28 +0100

    moved piped to mass storage to avoid small tmp

--- bash/blockchair/addresses.sh
+++ bash/blockchair/addresses.sh
@@ -1,20 +1,24 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 if [[ "$2" == "" ]]; then
-	echo "$(basename $0) INPUTSDIR OUTPUTSDIR NTHREADS" 1>&2
-	echo "Reads inputs in INPUTSDIR and outputs in OUTPUTSDIR and processes them using NTHREADS parallel sorts." 1>&2
-	echo "FILES MUST END WITH A NEWLINE. Fix them with \"sed -i -e '\$a\\' *\"." 1>&2
-	exit 1
+        echo "$(basename $0) INPUTSDIR OUTPUTSDIR NTHREADS" 1>&2
+        echo "Reads inputs in INPUTSDIR and outputs in OUTPUTSDIR and processes them using NTHREADS parallel sorts." 1>&2
+        echo "FILES MUST END WITH A NEWLINE. Fix them with \"sed -i -e '\$a\\' *\"." 1>&2
+        exit 1
 fi
 
 INPUTSDIR=$1
 OUTPUTSDIR=$2
 NTHREADS=$(expr $3 / 2)
 
-INPUTFILE=$(mktemp)
-bash extract.sh $INPUTSDIR $NTHREADS input address > $INPUTFILE &
+SCRIPTDIR=$(dirname $(realpath $0))
 
-OUTPUTFILE=$(mktemp)
-bash extract.sh $OUTPUTSDIR $NTHREADS output address > $OUTPUTFILE &
+mkfifo $INPUTSDIR/input.pipe
+bash $SCRIPTDIR/extract.sh $INPUTSDIR $NTHREADS input address > $INPUTSDIR/input.pipe &
 
-LC_ALL=C sort -S2G -m $INPUTFILE $OUTPUTFILE
+mkfifo $OUTPUTSDIR/output.pipe
+bash $SCRIPTDIR/extract.sh $OUTPUTSDIR $NTHREADS output address > $OUTPUTSDIR/output.pipe &
+
+LC_ALL=C sort -S2G -m $INPUTTMP.pipe $OUTPUTTMP.pipe
+
+rm -f $INPUTSDIR/input.pipe $OUTPUTSDIR/output.pipe
