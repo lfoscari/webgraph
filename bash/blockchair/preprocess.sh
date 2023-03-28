861883124b7d3e0cc894fdfc292223892675f4cd
Luigi Foscari <luigi.foscari@icloud.com>
2023-03-28 17:30:54 +0200

    explicit tmpdir

--- bash/blockchair/preprocess.sh
+++ bash/blockchair/preprocess.sh
@@ -14,24 +14,24 @@ OUTPUTSDIR=$2
 NTHREADS=$3
 
 SECONDS=0
-TMPDIR=$(mktemp -d -p "$(pwd)")
+_TMPDIR=$(mktemp -d -p "$(pwd)")
 
 echo "Extracting inputs"
-bash ~/transactiongraph/bash/blockchair/extract.sh $INPUTSDIR $NTHREADS input > inputs.tsv
+TMPDIR=$_TMPDIR bash ~/transactiongraph/bash/blockchair/extract.sh $INPUTSDIR $NTHREADS input > inputs.tsv
 
 echo "Extracting outputs"
-bash ~/transactiongraph/bash/blockchair/extract.sh $OUTPUTSDIR $NTHREADS output > outputs.tsv
+TMPDIR=$_TMPDIR bash ~/transactiongraph/bash/blockchair/extract.sh $OUTPUTSDIR $NTHREADS output > outputs.tsv
 
 echo "Extracting addresses"
-bash ~/transactiongraph/bash/blockchair/addresses.sh $INPUTSDIR $OUTPUTSDIR $NTHREADS > addresses.tsv
+TMPDIR=$_TMPDIR bash ~/transactiongraph/bash/blockchair/addresses.sh $INPUTSDIR $OUTPUTSDIR $NTHREADS > addresses.tsv
 
 echo "Extracting transactions"
-bash ~/transactiongraph/bash/blockchair/transactions.sh $INPUTSDIR $OUTPUTSDIR $NTHREADS > transactions.tsv
+TMPDIR=$_TMPDIR bash ~/transactiongraph/bash/blockchair/transactions.sh $INPUTSDIR $OUTPUTSDIR $NTHREADS > transactions.tsv
 
 echo "Computing address map"
-java -Djava.io.tmpdir="$TMPDIR" it.unimi.dsi.sux4j.mph.GOV3Function -b -s 10 address.map addresses.tsv
+TMPDIR=$_TMPDIR java -Djava.io.tmpdir="$TMPDIR" it.unimi.dsi.sux4j.mph.GOV3Function -b -s 10 address.map addresses.tsv
 
 echo "Computing transaction map"
-java -Djava.io.tmpdir="$TMPDIR" it.unimi.dsi.sux4j.mph.GOV3Function -b -s 10 transaction.map transactions.tsv
+TMPDIR=$_TMPDIR java -Djava.io.tmpdir="$TMPDIR" it.unimi.dsi.sux4j.mph.GOV3Function -b -s 10 transaction.map transactions.tsv
 
 echo "$((SECONDS / 60 / 60)) hours, $((SECONDS / 60 % 60)) minutes and $((SECONDS % 60)) seconds elapsed"
