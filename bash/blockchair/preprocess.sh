279a92bd6a08d97c25c5916f5c2a5bf5d6145fef
Luigi Foscari <luigi.foscari@icloud.com>
2023-03-28 16:44:43 +0200

    Revert "script refactoring"
    This reverts commit 26bff97ce136c0d42ee64f2843d9d41f30998cba.

--- bash/blockchair/preprocess.sh
+++ bash/blockchair/preprocess.sh
@@ -4,10 +4,8 @@ set -e
 set -o pipefail
 
 if [[ "$3" == "" ]]; then
-	"$(basename "$0") INPUTSDIR OUTPUTSDIR NTHREADS" 1>&2
-	echo "Reads inputs in INPUTSDIR and outputs in OUTPUTSDIR." 1>&2
-	echo "Extracts inputs, outputs, addresses and transaction." 1>&2
-	echo "Computes address and transaction maps." 1>&2
+	"$(basename $0) INPUTSDIR OUTPUTSDIR NTHREADS" 1>&2
+	echo "Reads inputs in INPUTSDIR and outputs in OUTPUTSDIR and creates inputs and outputs ready to be parsed, alongside address and transaction maps." 1>&2
 	exit 1
 fi
 
@@ -15,16 +13,16 @@ SECONDS=0
 TMPDIR=$(mktemp -d -p "$(pwd)")
 
 echo "Extracting inputs"
-bash ~/transactiongraph/bash/blockchair/extract.sh "$INPUTSDIR" "$NTHREADS" input > inputs.tsv
+bash ~/transactiongraph/bash/blockchair/extract.sh $INPUTSDIR $NTHREADS input > inputs.tsv
 
 echo "Extracting outputs"
-bash ~/transactiongraph/bash/blockchair/extract.sh "$OUTPUTSDIR" "$NTHREADS" output > outputs.tsv
+bash ~/transactiongraph/bash/blockchair/extract.sh $OUTPUTSDIR $NTHREADS output > outputs.tsv
 
 echo "Extracting addresses"
-bash ~/transactiongraph/bash/blockchair/addresses.sh "$INPUTSDIR" "$OUTPUTSDIR" "$NTHREADS" > addresses.tsv
+bash ~/transactiongraph/bash/blockchair/addresses.sh $INPUTSDIR $OUTPUTSDIR $NTHREADS > addresses.tsv
 
 echo "Extracting transactions"
-bash ~/transactiongraph/bash/blockchair/transactions.sh "$INPUTSDIR" "$OUTPUTSDIR" "$NTHREADS" > transactions.tsv
+bash ~/transactiongraph/bash/blockchair/transactions.sh $INPUTSDIR $OUTPUTSDIR $NTHREADS > transactions.tsv
 
 echo "Computing address map"
 java -Djava.io.tmpdir="$TMPDIR" it.unimi.dsi.sux4j.mph.GOV3Function -b -s 10 address.map addresses.tsv
