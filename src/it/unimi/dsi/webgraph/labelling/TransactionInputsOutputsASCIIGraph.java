77b08df7c7fc0ab6f9bd1006648482039be67883
Luigi Foscari <luigi.foscari@icloud.com>
2023-05-15 15:50:43 +0200

    typo

--- src/it/unimi/dsi/webgraph/labelling/TransactionInputsOutputsASCIIGraph.java
+++ src/it/unimi/dsi/webgraph/labelling/TransactionInputsOutputsASCIIGraph.java
@@ -60,7 +60,7 @@ public class TransactionInputsOutputsASCIIGraph extends ImmutableSequentialGraph
 	/**
 	 * The default label prototype.
 	 */
-	public static final Label DEFAULT_LABEL_PROTOTYPE = new MergeableFixedWidthLongListLabel("transaction-id", Long.SIZE);
+	public static final Label DEFAULT_LABEL_PROTOTYPE = new MergeableFixedWidthLongListLabel("transaction-id", Long.SIZE - 1);
 	/**
 	 * The default label mapping function.
 	 */
